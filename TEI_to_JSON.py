aaa52631805ec8beb2d330ff06a1ffa1f9acc6a9
David Flood <davidfloodii@gmail.com>
2021-06-18 10:38:28 +0100

    handle arbritrary correctors and nested encodings in <app> elems

--- TEI_to_JSON.py
+++ TEI_to_JSON.py
@@ -8,7 +8,27 @@ from from_tei import (get_file, pre_parse_cleanup,
                       parse, remove_unclear_tags,
                       tei_ns, get_verse_as_tuple)
 
-def tei_to_json(tei: str, output_dir, single_verse: str = None):
+def get_siglum(root: et._Element) -> str:
+    titles = root.xpath('//tei:title', namespaces={'tei': tei_ns})
+    for title in titles:
+        if title.get('n'):
+            siglum = title.get('n')
+            break
+    else:
+        siglum = ''
+        while siglum == '':
+            siglum = input('No siglum was found.\nPlease enter a witness ID: ')
+    return siglum
+
+def get_hands(root: et._Element) -> list:
+    rdgs = root.xpath('//tei:rdg', namespaces={'tei': tei_ns})
+    hands = []
+    for rdg in rdgs:
+        if rdg.get('hand') and rdg.get('hand') not in hands:
+            hands.append(rdg.get('hand'))
+    return hands
+
+def tei_to_json(tei: str, output_dir, single_verse: str):
     text = get_file(tei)
     text = pre_parse_cleanup(text)
     root = parse(text)
@@ -16,17 +36,15 @@ def tei_to_json(tei: str, output_dir, single_verse: str = None):
     text = et.tostring(root, encoding='unicode')
     text = remove_unclear_tags(text)
     root = parse(text)
-    # write_xml(root)
-    hands = ['firsthand', 'corrector']
-    siglum = root.xpath('//tei:title', namespaces={'tei': tei_ns})[0].get('n')
+    hands = get_hands(root)
+    siglum = get_siglum(root)
     metadata = {'id': siglum, 'siglum': siglum}
     verses = root.xpath(f'//tei:ab', namespaces={'tei': tei_ns})
     for verse in verses:
         ref = verse.get('n')
-        if single_verse and single_verse != ref:
+        if single_verse != '' and single_verse != ref:
             continue
         witnesses = get_verse_as_tuple(verse, hands=hands)
-        # print(witnesses)
         verse_as_dict = verse_to_dict(siglum, ref, witnesses)
         save_tx(verse_as_dict, ref, output_dir)
     if output_dir:
@@ -53,7 +71,7 @@ def main():
     else:
         output_dir = args.o
     try:
-        tei_to_json(TEI, output_dir)
+        tei_to_json(TEI, output_dir, '')
     except:
         print('The input file cannot be converted.')
 
